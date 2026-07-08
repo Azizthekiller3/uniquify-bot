4d29a781f1b3c47662a0efc8a7ec625d9fbce6a6
Azizthekiller3 <86946226+Azizthekiller3@users.noreply.github.com>
2026-07-08 20:09:48 +0530

    feat: register bot command menu (/start, /help, /chat, /delay, /purge)

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from os.path import exists
 
 from pyrogram import Client, enums
+from pyrogram.types import BotCommand
 
 if exists("./config.py"):
     from config import LOGGER, Config
@@ -8,6 +9,14 @@ else:
     from sample_config import LOGGER, Config
 
 from user import User
+
+BOT_COMMANDS = [
+    BotCommand("start", "Show the welcome message and help"),
+    BotCommand("help", "Explain how the bot works"),
+    BotCommand("chat", "Set the target chat, e.g. /chat -100xxxxxxxxxx"),
+    BotCommand("delay", "Set a delay between deletions, e.g. /delay 10"),
+    BotCommand("purge", "Find and delete duplicate media in the target chat"),
+]
 
 
 class Bot(Client):
@@ -29,6 +38,7 @@ class Bot(Client):
         await super().start()
         usr_bot_me = await self.get_me()
         self.set_parse_mode(enums.ParseMode.HTML)
+        await self.set_bot_commands(BOT_COMMANDS)
         self.LOGGER(__name__).info(
             f"Bot {usr_bot_me.first_name} (@{usr_bot_me.username}) started!"
         )
